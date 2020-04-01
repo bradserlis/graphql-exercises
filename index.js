61d6e0bdf55cae3b6112e71951d00360913337fa
bradserlis <bradserlis@yahoo.com>
2020-04-01 14:16:13 -0700

    First query

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing?')
 })
+
+const root = {
+    hello: () =>
+        "Hello, I'm Joe Pera"
+}
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
 
 app.listen(8080, () => {
     console.log('running server on port 8080/graphql');
