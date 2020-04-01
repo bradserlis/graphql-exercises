b86ee15e5926b8195d53664f168e9b3b29ff0ea4
bradserlis <bradserlis@yahoo.com>
2020-04-01 14:39:49 -0700

    Add mutation, create Friend

--- index.js
+++ index.js
@@ -5,8 +5,23 @@ import schema from './schema';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('GraphQL is amazing?')
-})
+    res.send('GraphQL is amazing?');
+});
+
+class Friend {
+    constructor(id, {
+        firstName, lastName, gender, language, email
+    }) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.language = language;
+        this.email = email;
+    }
+}
+
+const friendDatabase = {}
 
 const root = {
     friend: () => {
@@ -16,8 +31,14 @@ const root = {
             "lastName": "Pera",
             "gender": "Male",
             "language": "English",
-            "emails": [{ email: "no@no.com" }, { email: "another@me.com" }]
+            "email": "no@no.com"
         }
+    },
+
+    createFriend: ({ input }) => {
+        let id = require('crypto').randomBytes(10).toString('hex');
+        friendDatabase[id] = input;
+        return new Friend(id, input);
     }
 }
 
