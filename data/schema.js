98ff372575450681123f4b1ae1e17ecf016f18c8
bradserlis <bradserlis@yahoo.com>
2020-04-03 15:21:09 -0700

    Remove document from collection through mutation

--- data/schema.js
+++ data/schema.js
@@ -53,7 +53,8 @@ const typeDefs = `
 
     type Mutation {
         createFriend(input: FriendInput): Friend
-        updateFriend(input: FriendInput): Friend 
+        updateFriend(input: FriendInput): Friend
+        deleteFriend(id: ID!): String 
     }
 `;
 
